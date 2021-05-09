ff0bb10fbe12d85b9e04cae2c6b3c6cf09830254
skynet20beta <skynet2.0beta@gmail.com>
2021-05-09 15:55:00 +0300

    Добавьте файлы проекта.

---
+++ Flumen/StringExtensions.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Flumen
+{
+    /// <summary>
+    /// Standard BCL methods for strings wrapped in extension methods.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Wraps <see cref="string.IsNullOrEmpty"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string to test.</param>
+        /// <returns>Same as <see cref="string.IsNullOrEmpty"/>.</returns>
+        public static bool IsNullOrEmpty(this string? @string) =>
+            string.IsNullOrEmpty(@string);
+
+        /// <summary>
+        /// Wraps <see cref="string.IsNullOrWhiteSpace"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string to test.</param>
+        /// <returns>Same as <see cref="string.IsNullOrWhiteSpace"/>.</returns>
+        public static bool IsNullOrWhiteSpace(this string? @string) =>
+            string.IsNullOrWhiteSpace(@string);
+
+        /// <summary>
+        /// Wraps <see cref="Regex.IsMatch(string, string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string to search for a match.</param>
+        /// <param name="pattern">The regular expression pattern.</param>
+        /// <returns>Same as <see cref="Regex.IsMatch(string, string)"/>.</returns>
+        public static bool IsMatch(this string @string, string pattern) =>
+            Regex.IsMatch(@string, pattern);
+
+        /// <summary>
+        /// Wraps <see cref="bool.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="bool.Parse(string)"/>.</returns>
+        public static bool ToBoolean(this string @string) => 
+            bool.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="int.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="int.Parse(string)"/>.</returns>
+        public static int ToInt32(this string @string) => 
+            int.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="long.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="long.Parse(string)"/>.</returns>
+        public static long ToInt64(this string @string) => 
+            long.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="short.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="short.Parse(string)"/>.</returns>
+        public static short ToInt16(this string @string) => 
+            short.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="uint.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="uint.Parse(string)"/>.</returns>
+        public static uint ToUInt32(this string @string) => 
+            uint.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="ulong.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="ulong.Parse(string)"/>.</returns>
+        public static ulong ToUInt64(this string @string) => 
+            ulong.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="ushort.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="ushort.Parse(string)"/>.</returns>
+        public static ushort ToUInt16(this string @string) => 
+            ushort.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="byte.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="byte.Parse(string)"/>.</returns>
+        public static byte ToByte(this string @string) => 
+            byte.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="sbyte.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="sbyte.Parse(string)"/>.</returns>
+        public static sbyte ToSByte(this string @string) => 
+            sbyte.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="double.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="double.Parse(string)"/>.</returns>
+        public static double ToDouble(this string @string) => 
+            double.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="float.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="float.Parse(string)"/>.</returns>
+        public static float ToSingle(this string @string) => 
+            float.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="Guid.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="Guid.Parse(string)"/>.</returns>
+        public static Guid ToGuid(this string @string) => 
+            Guid.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="decimal.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="decimal.Parse(string)"/>.</returns>
+        public static decimal ToDecimal(this string @string) => 
+            decimal.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="TimeSpan.Parse(string)"/>.</returns>
+        public static TimeSpan ToTimeSpan(this string @string) => 
+            TimeSpan.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="DateTime.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="DateTime.Parse(string)"/>.</returns>
+        public static DateTime ToDateTime(this string @string) => 
+            DateTime.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="DateTimeOffset.Parse(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="DateTimeOffset.Parse(string)"/>.</returns>
+        public static DateTimeOffset ToDateTimeOffset(this string @string) => 
+            DateTimeOffset.Parse(@string);
+
+        /// <summary>
+        /// Wraps <see cref="Enum.Parse&lt;TEnum&gt;(string)"/> for using as extension. 
+        /// </summary>
+        /// <param name="string">The string containing value to convert.</param>
+        /// <returns>Same as <see cref="Enum.Parse&lt;TEnum&gt;(string)"/>.</returns>
+        public static TEnum To<TEnum>(this string @string)
+            where TEnum : struct, Enum =>
+            Enum.Parse<TEnum>(@string);
+    }
+}
---
+++ Flumen/TimeSpanExtensions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Flumen
+{
+    /// <summary>
+    /// Standard BCL methods for <see cref="TimeSpan"/> wrapped in extension methods.
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        private const long DAYS_PER_WEEK = 7;
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> that represents a specified number of weeks. 
+        /// </summary>
+        /// <param name="value">A number of weeks.</param>
+        /// <returns><see cref="TimeSpan"/> with a specified number of weeks.</returns>
+        public static TimeSpan Week(this double value) =>
+            DAYS_PER_WEEK * value.Days();
+
+        /// <summary>
+        /// Returns a <see cref="TimeSpan"/> that represents a specified number of weeks. 
+        /// </summary>
+        /// <param name="value">A number of weeks.</param>
+        /// <returns><see cref="TimeSpan"/> with a specified number of weeks.</returns>
+        public static TimeSpan Weeks(this double value) =>
+            DAYS_PER_WEEK * value.Days();
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromDays"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of days.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromDays"/>.</returns>
+        public static TimeSpan Day(this double value) =>
+            TimeSpan.FromDays(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromDays"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of days.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromDays"/>.</returns>
+        public static TimeSpan Days(this double value) =>
+            TimeSpan.FromDays(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromHours"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of hours.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromHours"/>.</returns>
+        public static TimeSpan Hour(this double value) =>
+            TimeSpan.FromHours(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromHours"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of hours.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromHours"/>.</returns>
+        public static TimeSpan Hours(this double value) =>
+            TimeSpan.FromHours(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromMinutes"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of minutes.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromMinutes"/>.</returns>
+        public static TimeSpan Minute(this double value) =>
+            TimeSpan.FromMinutes(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromMinutes"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of minutes.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromMinutes"/>.</returns>
+        public static TimeSpan Minutes(this double value) =>
+            TimeSpan.FromMinutes(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromSeconds"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of seconds.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromSeconds"/>.</returns>
+        public static TimeSpan Second(this double value) =>
+            TimeSpan.FromSeconds(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromSeconds"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of seconds.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromSeconds"/>.</returns>
+        public static TimeSpan Seconds(this double value) =>
+            TimeSpan.FromSeconds(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromMilliseconds"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of milliseconds.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromMilliseconds"/>.</returns>
+        public static TimeSpan Millisecond(this double value) =>
+            TimeSpan.FromMilliseconds(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromMilliseconds"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of milliseconds.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromMilliseconds"/>.</returns>
+        public static TimeSpan Milliseconds(this double value) =>
+            TimeSpan.FromMilliseconds(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromTicks"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of ticks.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromTicks"/>.</returns>
+        public static TimeSpan Tick(this long value) =>
+            TimeSpan.FromTicks(value);
+
+        /// <summary>
+        /// Wraps <see cref="TimeSpan.FromTicks"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number of ticks.</param>
+        /// <returns>Same as <see cref="TimeSpan.FromTicks"/>.</returns>
+        public static TimeSpan Ticks(this long value) =>
+            TimeSpan.FromTicks(value);
+    }
+}
