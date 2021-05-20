e34143269f5c234e33bd86cae84c311aaf880c5c
skynet20beta <skynet2.0beta@gmail.com>
2021-05-20 23:00:24 +0300

    Math and Double methods added

---
+++ Flumen/DoubleExtensions.cs
@@ -0,0 +1,80 @@
+﻿namespace Flumen
+{
+    /// <summary>
+    /// Standard BCL methods for <see cref="double"/> wrapped in extension methods.
+    /// </summary>
+    public static class DoubleExtensions
+    {
+        /// <summary>
+        /// Wraps <see cref="double.IsNaN"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsNaN"/>.</returns>
+        public static bool IsNaN(this double value) =>
+            double.IsNaN(value);
+
+        /// <summary>
+        /// Wraps <see cref="double.IsInfinity"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsInfinity"/>.</returns>
+        public static bool IsInfinity(this double value) =>
+            double.IsInfinity(value);
+
+        /// <summary>
+        /// Wraps <see cref="double.IsPositiveInfinity"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsPositiveInfinity"/>.</returns>
+        public static bool IsPositiveInfinity(this double value) =>
+            double.IsPositiveInfinity(value);
+
+        /// <summary>
+        /// Wraps <see cref="double.IsNegativeInfinity"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsNegativeInfinity"/>.</returns>
+        public static bool IsNegativeInfinity(this double value) =>
+            double.IsNegativeInfinity(value);
+
+        /// <summary>
+        /// Wraps <see cref="double.IsFinite"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsFinite"/>.</returns>
+        public static bool IsFinite(this double value) =>
+            double.IsFinite(value);
+
+        /// <summary>
+        /// Wraps <see cref="double.IsNormal"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsNormal"/>.</returns>
+        public static bool IsNormal(this double value) =>
+            double.IsNormal(value);
+
+        /// <summary>
+        /// Wraps <see cref="double.IsSubnormal"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsSubnormal"/>.</returns>
+        public static bool IsSubnormal(this double value) =>
+            double.IsSubnormal(value);
+
+        /// <summary>
+        /// Wraps <see cref="double.IsNegative"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsNegative"/>.</returns>
+        public static bool IsNegative(this double value) =>
+            double.IsNegative(value);
+
+        /// <summary>
+        /// Determines whether specified value is positive based on <see cref="double.IsNegative"/>. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <returns>Same as <see cref="double.IsNegative"/>.</returns>
+        public static bool IsPositive(this double value) =>
+            !double.IsNegative(value);
+    }
+}
---
+++ Flumen/MathExtensions.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace Flumen
+{
+    /// <summary>
+    /// Standard BCL methods from Math and some additional methods wrapped in extension methods.
+    /// </summary>
+    public static class MathExtensions
+    {
+        /// <summary>
+        /// Wraps <see cref="Math.Sin"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Angle.</param>
+        public static double Sin(this double value) =>
+            Math.Sin(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Cos"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Angle.</param>
+        public static double Cos(this double value) =>
+            Math.Cos(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Tan"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Angle.</param>
+        public static double Tan(this double value) =>
+            Math.Tan(value);
+
+        /// <summary>
+        /// Calculates cotangent as tan(pi / 2 - value). 
+        /// </summary>
+        /// <param name="value">Angle.</param>
+        public static double Ctan(this double value) => 
+            Math.Tan(Math.PI / 2.0 - value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Asin"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number representing sine.</param>
+        public static double Asin(this double value) =>
+            Math.Asin(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Acos"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number representing cosine.</param>
+        public static double Acos(this double value) =>
+            Math.Acos(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Atan"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">A number representing tangent.</param>
+        public static double Atan(this double value) =>
+            Math.Atan(value);
+
+        /// <summary>
+        /// Calculates arccotangent as pi / 2 - arctg(a). 
+        /// </summary>
+        /// <param name="value">A number representing cotangent.</param>
+        public static double Actan(this double value) =>
+            Math.PI/2 - Math.Atan(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Abs(double)"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Abs(this double value) =>
+            Math.Abs(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Cbrt"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Cbrt(this double value) =>
+            Math.Cbrt(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Sqrt"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Sqrt(this double value) =>
+            Math.Sqrt(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Exp"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Exp(this double value) =>
+            Math.Exp(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Log(double)"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Log(this double value) =>
+            Math.Log(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Log10"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Log10(this double value) =>
+            Math.Log10(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Floor(double)"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Floor(this double value) =>
+            Math.Floor(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Ceiling(double)"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Ceiling(this double value) =>
+            Math.Ceiling(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Truncate(double)"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Truncate(this double value) =>
+            Math.Truncate(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Round(double)"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Round(this double value) =>
+            Math.Round(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Sign(double)"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        public static double Sign(this double value) =>
+            Math.Sign(value);
+
+        /// <summary>
+        /// Wraps <see cref="Math.Pow"/> for using as extension. 
+        /// </summary>
+        /// <param name="value">Double value.</param>
+        /// <param name="power">Power.</param>
+        public static double Pow(this double value, double power) =>
+            Math.Pow(value, power);
+    }
+}
